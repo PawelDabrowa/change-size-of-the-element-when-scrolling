f6b843580ad74b831a982de5201aebefd07792a0
Pawel Dabrowa <paweldabrowa@pawels-air.chwv.local>
2019-05-01 15:49:09 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const scrollElement = document.createElement('div');
+document.querySelector('body').appendChild(scrollElement);
+let grow = true;
+
+let size = 1;
+scrollElement.style.height = size + 'px';
+
+
+
+changeHstyle = () => {
+    if (size >= window.innerWidth * 1) {
+        grow = !grow;
+    } else if (size <= 0) {
+        grow = !grow;
+    }
+
+
+    if (grow) {
+        size += 5
+        scrollElement.style.height = size + "px";
+        scrollElement.style.backgroundColor = "red";
+
+    }
+
+    else {
+        size -= 5
+        scrollElement.style.height = size + "px";
+        scrollElement.style.backgroundColor = "green";
+
+    }
+
+}
+
+window.addEventListener('scroll', changeHstyle);
